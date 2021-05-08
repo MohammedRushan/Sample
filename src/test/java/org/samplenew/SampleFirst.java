92f035e3a499c5a1218f319d89419d874ea56a0f
Muhajir M.R <mohamemr@amazon.com>
2021-05-08 07:26:22 -0700

    Sample program

---
+++ src/test/java/org/samplenew/SampleFirst.java
@@ -0,0 +1,22 @@
+package org.samplenew;
+
+import java.awt.event.KeyEvent;
+
+public class SampleFirst {
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a<=10)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+	}
+	
+	
+	 
+    
+}
